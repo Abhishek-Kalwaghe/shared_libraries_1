5b6147f48ce1f6fa81e5af5a6fc9445028de30ae
Abhishek-Kalwaghe <127833583+Abhishek-Kalwaghe@users.noreply.github.com>
2023-03-20 19:05:40 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -11,3 +11,7 @@ def deploy(pipe_name,ip,context)
   sh "scp /var/lib/jenkins/workspace/${pipe_name}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}"
 
 }
+def testing(var)
+{
+  sh "java -jar /var/lib/jenkins/workspace/${var}/testing.jar"
+}
